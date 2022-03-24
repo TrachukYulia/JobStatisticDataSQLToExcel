06ea70485207ce54540d75ec19e0e3bf6db2d711
Julia Trachuk <trach@Juliya.jul>
2022-03-24 17:51:00 +0200

    Add project files.

---
+++ JobStatisticDataSQLToExcel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TransferToExcel;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace JobStatisticDataSQLToExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var excelData = new ExcelData();
+            SqlConnection connection = DBConnection.GetDBConnection();
+
+            connection.Open();
+            try
+            {
+                excelData.TransferToExcel(connection);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+            Console.Read();
+        }
+    }
+}
